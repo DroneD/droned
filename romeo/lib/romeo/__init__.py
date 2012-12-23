51498e3fda26022c72fc469d268f2dbfdbe8d10f
Justin Venus <justin.venus@gmail.com>
2012-12-22 19:17:07 -0600

    minor update that had been bothering me.

--- romeo/lib/romeo/__init__.py
+++ romeo/lib/romeo/__init__.py
@@ -114,7 +114,9 @@ def reload(datadir=None):
 
     from romeo.directives import Preprocessor
     pp = Preprocessor(datadir)
-    for f in _glob.glob('%s/*.yaml' % (datadir,)):
+    for f in _glob.glob('%s/*' % (datadir,)):
+        if not f.endswith('.yaml') or not f.endswith('.yml'):
+            continue
         try:
             fd = open(f, 'r')
             outstr = pp.pre_process(fd,f)
